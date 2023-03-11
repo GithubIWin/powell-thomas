2c1d2ac90a943f8d5a97a79515f1c25819986153
GithubIWin <ivinvarghese00@gmail.com>
2023-03-11 23:34:04 +0530

    case details

--- src/components/StudeyDetails.jsx
+++ src/components/StudeyDetails.jsx
@@ -1,6 +1,46 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
 import "./style.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+
+const SectionPart = (props) => {
+  const caseStudy = props.case;
+  return (
+    <div>
+      <h2 className="detailSubTitle">{caseStudy.subHead}</h2>
+      {caseStudy.details ? (
+        <p className="detailContent" key={caseStudy.key}>
+          {caseStudy.details}
+        </p>
+      ) : null}
+      {caseStudy.subSections
+        ? caseStudy.subSections.map((iter, key) => (
+            <div>
+              <h3>{iter.subHead}</h3>
+              {iter.details.map((iter, key) => (
+                <p className="detailContent" key={key}>
+                  {iter}
+                </p>
+              ))}
+              <Container>
+                <Row>
+                  {iter.images && iter.images.length > 0
+                    ? iter.images.map((img, key) => (
+                        <Col xs={12} sm="12" md="6" lg="4">
+                          <Image src={img.src} alt={img.alt} rounded />
+                        </Col>
+                      ))
+                    : null}
+                </Row>
+              </Container>
+            </div>
+          ))
+        : null}
+    </div>
+  );
+};
 
 const StudeyDetails = (props) => {
   const { caseStudy, setOpenStudyDetail } = props;
@@ -17,15 +57,10 @@ const StudeyDetails = (props) => {
       ))}
 
       {caseStudy.contents.map((iter, key) => (
-        <div>
-          <h2 className="detailSubTitle">{iter.subHead}</h2>
-          <p className="detailContent" key={key}>
-            {iter.details}
-          </p>
-        </div>
+        <SectionPart case={iter} key={key} />
       ))}
 
-      <button  className="cardButton" onClick={HandleClick}>
+      <button className="cardButton" onClick={HandleClick}>
         Go Back
       </button>
     </div>
--- src/static.js
+++ src/static.js
@@ -1,3 +1,6 @@
+import ayurvedic1 from "./resources/casestudiesImg/ayurvedicImg1.jpg";
+import ayurvedic2 from "./resources/casestudiesImg/ayurvedicImg2.jpg";
+
 export const caseStudiesData = [
   {
     thumbnail: "Reduced LinkedIn Ads CPL by 23% for a B2B SaaS Client",
@@ -29,89 +32,112 @@ export const caseStudiesData = [
     ],
   },
   {
-    thumbnail: "Reduced LinkedIn Ads CPL by 23% for a B2B SaaS Client",
-    title: "Reduced LinkedIn Ads CPL by 23% for a B2B SaaS Client",
+    thumbnail: "Return of 10.65 For Every Rupee Spent for an Ayurvedic Brand",
+    title: "Return of 10.65 For Every Rupee Spent for an Ayurvedic Brand",
     description:
-      "Blusteak recently ran a series of Linkedin Lead Generation Ads for a Quality Management SaaS platform. The client has a lot of products catering to different industries, and it continuously faced a high CPL (Cost Per Lead) for its ads. We worked on multiple targeting combinations. And within a month, the CPL (Cost Per Lead) came down by 23% of what it had been earlier. The number of quality leads also went up from 54% to a spectacular 80%. Read this case study to find out how Blusteak did it.",
+      "Blusteak recently collaborated with an ayurvedic product brand to help them generate more sales with the help of a targeted campaign. We saw a jump of 1448% in sales through the online store within a month of launching the campaign.",
     details: [
-      "Blusteak had a colourful start to the year, marketing Kerala Literature Festival (KLF), the second-largest literature festival in Asia. We created many interesting campaigns and content that resulted in a huge growth of its social media channels. We also shared fun activities and promoted ongoing updates to keep the audience updated on everything.",
-      "Many of the Reels we created went over 100K and 200K views; a first for this page.",
-      "And within 2 months of posting, the IG page saw a 6x increase in engagement and a 100% growth in followers. We achieved a collective view count of over 13 Lakhs with only 29 Reels.",
-      "Read this case study to find out how Blusteak did it.",
+      "Blusteak recently collaborated with an ayurvedic product brand to help them generate more sales with the help of a targeted campaign. We saw a jump of 1448% in sales through the online store within a month of launching the campaign.",
+      "If you’re wondering how we did that, keep reading!",
     ],
     contents: [
       {
-        subHead: "About the Brand ",
+        subHead: "About the Brand",
         details:
-          "Our client has a suite of cloud-based quality management solutions to help businesses ensure consistent quality. The products are all connected together to help teams maintain consistency across the organization. It has solutions for various industries, from F&B to automotive and healthcare to aerospace and defense. And this variety made ad targeting tricky.",
+          "Alpha Ayurveda takes traditional ayurvedic recipes and formulas and incorporates them into natural, sustainable processes to create products for weight gain, hair fall, gas troubles, etc. The company is Ayurveda-based and run by experts in the ancient healing art. They’re on a mission to make the world a healthier place – and they’re making it possible with the help of Ayurveda.",
       },
       {
         subHead: "Our Challenge ",
         details:
-          "We had to bring down LinkedIn ads CPL while targeting multiple personas of companies across several industries. And the leads needed to be relevant, of course. ",
+          "Since we are advertising health-related products, it isn’t easy to do so according to Facebook policies and guidelines. We also had to identify skinny people from PAN Kerala. There are a lot of competitors around us, and since COD is not there, convincing people in Kerala to buy the product can be difficult. Hence, we were required to come up with impactful content.",
       },
       {
-        subHead: "The Result",
+        subHead: "The Ultimate Solution",
+        details: "",
+        subSections: [
+          {
+            subHead: "100 days Slim to Fit Challenge",
+            details: [
+              "The 100-day Slim to Fit Challenge was an offer we created wherein we gave a product for 499 rupees. The challenge aimed to help skinny people get fit within 100 days.",
+              "We reached out to influencers and asked them to introduce the offer after trying the product themselves. The campaign lasted for five days, and anyone could participate. We created video ads, creatives, and stories to support the Challenge.",
+              "630 people participated in the challenge and got the product at 499 rupees.",
+            ],
+            images: [
+              {
+                src: ayurvedic1,
+                alt: "ayurvedic 100 day slim img",
+              },
+              {
+                src: ayurvedic2,
+                alt: "ayurvedic 100 day slim img",
+              },
+            ],
+          },
+          {
+            subHead: "Targeted campaign",
+            details: [
+              "We created a lookalike audience of people who had watched our video, people who had already purchased something from us, those who had added something to their cart, and website visitors. ",
+              "We included people who had watched the video for more than 50% of our retargeting audience. We also did interest targeting of people who already had some interests tested by one ad set.",
+            ],
+          },
+          {
+            subHead: "Targeted campaign",
+            details: [
+              "We created a lookalike audience of people who had watched our video, people who had already purchased something from us, those who had added something to their cart, and website visitors.",
+              "We included people who had watched the video for more than 50% of our retargeting audience. We also did interest targeting of people who already had some interests tested by one ad set.",
+            ],
+          },
+          {
+            subHead: "Niched Targeting",
+            details: [
+              "We used Malayalam content (UGC) to appeal to the Malayalam-speaking audience. We created separate ad campaigns for males and females. Also, to make the campaign more cost-effective, we only used one influencer video that would target both genders.",
+            ],
+          },
+          {
+            subHead: "Strong funneling",
+            details: [
+              "We created dynamic graphics to test out what works best. Then, we started WhatsApp targeting and found that they work best when targeting broad audiences and web visitors. Lastly, we worked on keeping customers engaged for 100 days which increased our retention rate significantly.",
+            ],
+          },
+        ],
+      },
+      {
+        subHead: "The Ultimate Result",
         details:
-          "Our client has a suite of cloud-based quality management solutions to help businesses ensure consistent quality. The products are all connected together to help teams maintain consistency across the organization. It has solutions for various industries, from F&B to automotive and healthcare to aerospace and defense. And this variety made ad targeting tricky.",
+          "The campaign generated a total of 2,66,273 in sales, with online sales accounting for Rs. 1,94,453 and WhatsApp Sales standing at 71,820. We spent a total amount of Rs. 25,000 on our ad campaign and saw a return of 10.65 for every rupee spent (ROAS).",
+      },
+      {
+        subHead: "Conclusion",
+        details:
+          "Targeting the right audience would make your ads more effective. As there are so many media platforms, getting your message across to the people who are likely to buy your product or service can be hard. You must understand where your prospects are to reach them. This is where a targeted ad campaign comes in.",
       },
     ],
   },
   {
-    thumbnail: "Reduced LinkedIn Ads CPL by 23% for a B2B SaaS Client",
-    title: "Reduced LinkedIn Ads CPL by 23% for a B2B SaaS Client",
+    thumbnail:
+      "How Blusteak Converted Over 50% of the Students at a Technology School",
+    title:
+      "How Blusteak Converted Over 50% of the Students at a Technology School",
     description:
-      "Blusteak recently ran a series of Linkedin Lead Generation Ads for a Quality Management SaaS platform. The client has a lot of products catering to different industries, and it continuously faced a high CPL (Cost Per Lead) for its ads. We worked on multiple targeting combinations. And within a month, the CPL (Cost Per Lead) came down by 23% of what it had been earlier. The number of quality leads also went up from 54% to a spectacular 80%. Read this case study to find out how Blusteak did it.",
+      "Blusteak recently collaborated with a startup ecosystem generating company in Kerala. The marketing department launched Facebook and Google ads and reached out to influencers to boost lead generation. Within a month, Blusteak’s team was able to convert 18 leads out of a total of 30 students, and more are to be added. This case study proves how they have done it. Let’s have a run through their journey.",
     details: [
-      "Blusteak had a colourful start to the year, marketing Kerala Literature Festival (KLF), the second-largest literature festival in Asia. We created many interesting campaigns and content that resulted in a huge growth of its social media channels. We also shared fun activities and promoted ongoing updates to keep the audience updated on everything.",
-      "Many of the Reels we created went over 100K and 200K views; a first for this page.",
-      "And within 2 months of posting, the IG page saw a 6x increase in engagement and a 100% growth in followers. We achieved a collective view count of over 13 Lakhs with only 29 Reels.",
-      "Read this case study to find out how Blusteak did it.",
+      "Blusteak recently collaborated with a startup ecosystem generating company in Kerala. The marketing department launched Facebook and Google ads and reached out to influencers to boost lead generation. Within a month, Blusteak’s team was able to convert 18 leads out of a total of 30 students, and more are to be added. This case study proves how they have done it. Let’s have a run through their journey.",
     ],
     contents: [
       {
-        subHead: "About the Brand ",
+        subHead: "About the brand",
         details:
-          "Our client has a suite of cloud-based quality management solutions to help businesses ensure consistent quality. The products are all connected together to help teams maintain consistency across the organization. It has solutions for various industries, from F&B to automotive and healthcare to aerospace and defense. And this variety made ad targeting tricky.",
+          "Our client is building a strong startup ecosystem in Kerala. They mold entrepreneurs, build startups from scratch, and take them to a highly scalable level, thereby creating more job opportunities and facilitating remarkable economic growth in our state. Their aim is to build 140 startups in Kerala by 2025 to shape and strengthen the startup ecosystem. They have already launched 10 startups, and the remaining 130 will be launched in the upcoming years. Technology school is one of their startups, and it was a promising platform with 100% placement for students who have completed or not can join this. It helps a lot of students who are interested in learning more about tech-related programs and more about the latest and greatest technology.",
       },
       {
-        subHead: "Our Challenge ",
+        subHead: "Brand Concern",
         details:
-          "We had to bring down LinkedIn ads CPL while targeting multiple personas of companies across several industries. And the leads needed to be relevant, of course. ",
+          "Our client wanted Blusteak to handle their lead generation and conversation through Instagram, Facebook, and YouTube. They were having issues dealing with generating leads for technology schools as they were doing paid ads at that time, and it wasn’t giving them what they had expected. They didn’t have a great time through their social media interactions with influencers. So, we had to look forward to catching up with influencers and launching engaging paid ads.",
       },
       {
-        subHead: "The Result",
+        subHead: "Our challenge",
         details:
-          "Our client has a suite of cloud-based quality management solutions to help businesses ensure consistent quality. The products are all connected together to help teams maintain consistency across the organization. It has solutions for various industries, from F&B to automotive and healthcare to aerospace and defense. And this variety made ad targeting tricky.",
-      },
-    ],
-  },
-  {
-    thumbnail: "Reduced LinkedIn Ads CPL by 23% for a B2B SaaS Client",
-    title: "Reduced LinkedIn Ads CPL by 23% for a B2B SaaS Client",
-    description:
-      "Blusteak recently ran a series of Linkedin Lead Generation Ads for a Quality Management SaaS platform. The client has a lot of products catering to different industries, and it continuously faced a high CPL (Cost Per Lead) for its ads. We worked on multiple targeting combinations. And within a month, the CPL (Cost Per Lead) came down by 23% of what it had been earlier. The number of quality leads also went up from 54% to a spectacular 80%. Read this case study to find out how Blusteak did it.",
-    details: [
-      "Blusteak had a colourful start to the year, marketing Kerala Literature Festival (KLF), the second-largest literature festival in Asia. We created many interesting campaigns and content that resulted in a huge growth of its social media channels. We also shared fun activities and promoted ongoing updates to keep the audience updated on everything.",
-      "Many of the Reels we created went over 100K and 200K views; a first for this page.",
-      "And within 2 months of posting, the IG page saw a 6x increase in engagement and a 100% growth in followers. We achieved a collective view count of over 13 Lakhs with only 29 Reels.",
-      "Read this case study to find out how Blusteak did it.",
-    ],
-    contents: [
-      {
-        subHead: "About the Brand ",
-        details:
-          "Our client has a suite of cloud-based quality management solutions to help businesses ensure consistent quality. The products are all connected together to help teams maintain consistency across the organization. It has solutions for various industries, from F&B to automotive and healthcare to aerospace and defense. And this variety made ad targeting tricky.",
-      },
-      {
-        subHead: "Our Challenge ",
-        details:
-          "We had to bring down LinkedIn ads CPL while targeting multiple personas of companies across several industries. And the leads needed to be relevant, of course. ",
-      },
-      {
-        subHead: "The Result",
-        details:
-          "Our client has a suite of cloud-based quality management solutions to help businesses ensure consistent quality. The products are all connected together to help teams maintain consistency across the organization. It has solutions for various industries, from F&B to automotive and healthcare to aerospace and defense. And this variety made ad targeting tricky.",
+          "Within a limited time, we can’t grow a brand whose social media engagements are not on the rise and whose brand awareness is not as pleasing for the audience to believe and focus their attention on. Our biggest challenge was creating a brand quality for the influencers, especially those in tech education. As technology schools are not active in giving a good social media appearance and brand development, influencers would doubt how relevant this is. Will their audience has any use for this?         However, our team found and convinced some great influencers whose audience found our client’s technology school apt for their further education. Since previously paid ads weren’t doing well, we thought of coming up with new ideas, and they worked successfully.",
       },
     ],
   },

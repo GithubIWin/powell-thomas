786d92050cd54b65b2e1c0a4e3da997f8456deb4
GithubIWin <ivinvarghese00@gmail.com>
2023-03-13 18:49:45 +0530

    added all case studies

--- src/components/StudeyDetails.jsx
+++ src/components/StudeyDetails.jsx
@@ -10,34 +10,108 @@ const SectionPart = (props) => {
   return (
     <div>
       <h2 className="detailSubTitle">{caseStudy.subHead}</h2>
-      {caseStudy.details ? (
-        <p className="detailContent" key={caseStudy.key}>
-          {caseStudy.details}
-        </p>
-      ) : null}
+      {caseStudy.details
+        ? caseStudy.details.map((iter, key) => (
+            <p className="detailContent" key={key}>
+              {iter}
+            </p>
+          ))
+        : null}
       {caseStudy.subSections
         ? caseStudy.subSections.map((iter, key) => (
             <div>
-              <h3>{iter.subHead}</h3>
+              <h3 className="subSubTitle">{iter.subHead}</h3>
               {iter.details.map((iter, key) => (
                 <p className="detailContent" key={key}>
                   {iter}
                 </p>
               ))}
-              <Container>
-                <Row>
-                  {iter.images && iter.images.length > 0
-                    ? iter.images.map((img, key) => (
-                        <Col xs={12} sm="12" md="6" lg="4">
-                          <Image src={img.src} alt={img.alt} rounded />
-                        </Col>
-                      ))
-                    : null}
-                </Row>
-              </Container>
+              {iter.images && iter.images.length > 0 ? (
+                <Container>
+                  <Row>
+                    {iter.images.map((img, key) => (
+                      <Col>
+                        <Image
+                          className="imgStyle"
+                          src={img.src}
+                          alt={img.alt}
+                          rounded
+                        />
+                        {img.caption && (
+                          <p>
+                            <i>
+                              <b>{img.caption}</b>
+                            </i>
+                          </p>
+                        )}
+                      </Col>
+                    ))}
+                  </Row>
+                </Container>
+              ) : null}
+              {iter.subSections
+                ? iter.subSections.map((section, key) => (
+                    <div>
+                      <h4 className="subheading">{section.subHead}</h4>
+                      {section.details.map((iter, key) => (
+                        <p className="detailContent" key={key}>
+                          {iter}
+                        </p>
+                      ))}
+                      {section.images && section.images.length > 0 ? (
+                        <Container>
+                          <Row>
+                            {section.images.map((img, key) => (
+                              <Col>
+                                <Image
+                                  className="imgStyle"
+                                  src={img.src}
+                                  alt={img.alt}
+                                  rounded
+                                />
+                                {img.caption && (
+                                  <p>
+                                    <i>
+                                      <b>{img.caption}</b>
+                                    </i>
+                                  </p>
+                                )}
+                              </Col>
+                            ))}
+                          </Row>
+                        </Container>
+                      ) : null}
+                    </div>
+                  ))
+                : null}
             </div>
           ))
         : null}
+      {caseStudy.images && caseStudy.images.length > 0 ? (
+        <Container>
+          <Row>
+            {caseStudy.images.map((img, key) => (
+              <Col>
+                <Image
+                  className="imgStyle"
+                  src={img.src}
+                  alt={img.alt}
+                  rounded
+                />
+                {img.caption && (
+                  <p>
+                    <p>
+                      <i>
+                        <b>{img.caption}</b>
+                      </i>
+                    </p>
+                  </p>
+                )}
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      ) : null}
     </div>
   );
 };

cfb5179135f3290ec790c10a2519c70f70d6df1f
GithubIWin <ivinvarghese00@gmail.com>
2023-03-25 16:07:52 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import Homepage from './components/Homepage';
+import logo from "./logo.svg";
+import "./App.css";
+import Homepage from "./components/Homepage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/ContactForm";
+import CaseContainer from "./components/CaseContainer";
+import HeaderSection from "./components/HeaderSection";
 
 function App() {
   return (
-    <div className="App">
-     <Homepage/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <HeaderSection />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/caseStudies" element={<CaseContainer />} />
+          <Route path="/about" element={<About />} />
+          {/* <Route path="/contact" element={<Contact />} /> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
-import powellbg from "../resources/powell-bg.png";
+import powellbg from "../resources/powellSq.jpg";
 import "./style.css";
 
 const Profile = () => {
@@ -12,41 +12,54 @@ const Profile = () => {
       <Row className="justify-content-md-center ">
         <Col lg="9">
           <Row className="justify-content-md-center ">
-            <Col className="colProfile">
-              <Image
-                src={powellbg}
-                alt="powell-thomas-profile-pic"
-                className="myImg"
-                rounded
-              />
-            </Col>
-            <Col className="colProfile parentCenter">
-              <div>
-                <p className="nameTitle">
-                  Hey, <br /> Powell Thomas here!!
-                </p>
-                <p>
-                  {" "}
-                  Greetings! I'm Powell, a seasoned performance marketing
-                  professional with over 4 years of experience managing 20+
-                  clients across 5 continents and diverse domains. With an
-                  overall budget of 5.8 Cr+ and generating 86 Cr+ in revenue, I
-                  have honed my expertise in executing comprehensive marketing
-                  strategies across all platforms.
-                </p>
-                <p>
-                  {" "}
-                  As a leader, I have successfully managed a 10-member team,
-                  ensuring the successful execution of campaigns and
-                  simultaneously managing 8+ clients on social media and paid
-                  channels with a monthly ad spend of 1.5 Cr.{" "}
-                </p>
-                <p>
-                  Proficient in HubSpot, Salesforce, Gartner, and Seamelss.ai, I
-                  create effective workflows for lead nurturing and onboarding,
-                  driving business growth and delivering exceptional results.
-                </p>
+            <Col lg="7" className="colProfile parentCenter">
+              <div className="header_content">
+                <div className="header_section">
+                  <h2>Hello, I Am</h2>
+                  <h1>Powell Thomas</h1>
+                  <p>
+                    Proven Success in Performance Marketing for Your Business
+                    Needs. Let's Take Your Business to New Heights.
+                  </p>
+                  <div className="header_buttons">
+                    <a href="/caseStudies" className="bttn bttn-outline">
+                      View My Works
+                    </a>
+                  </div>
+                </div>
               </div>
+            </Col>
+            <Col lg="5" className="colProfile ">
+              <Row className="imgSection">
+                <Col>
+                  <Image
+                    src={powellbg}
+                    alt="powell-thomas-profile-pic"
+                    className="myImg"
+                  />
+                  <div class="social-menu">
+                    <ul>
+                      <li>
+                        <a
+                          href="https://www.linkedin.com/in/powell-thomas-bbbab0131/"
+                          target="blank"
+                        >
+                          <i class="fab fa-linkedin-in"></i>
+                        </a>
+                      </li>
+                      <li><a href="https://www.instagram.com/imsanketbodke/" target="blank"><i class="fab fa-instagram"></i></a></li>
+                      <li>
+                        <a
+                          href="tmspowell1@gmail.com"
+                          target="blank"
+                        >
+                          <i class="fas fa-phone-volume"></i>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </Col>
+              </Row>
             </Col>
           </Row>
         </Col>
